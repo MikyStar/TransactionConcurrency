1a29b6c7f974f5476f2e9d13d096168a5ad4e617
Unknown <mike.aubenas@gmail.com>
2018-09-29 15:59:46 +0200

    Add inner class OrederedTransaction

--- src/core/TransactionMatrix.java
+++ src/core/TransactionMatrix.java
@@ -1,23 +1,57 @@
 package core;
 
 import com.sun.istack.internal.NotNull;
+import com.sun.xml.internal.ws.api.message.ExceptionHasMessage;
 
 import java.util.ArrayList;
 
 public class TransactionMatrix
 {
-    private ArrayList<ArrayList<Element>> matrix;
+    private ArrayList<OrderedTransaction> transactionMatrix;
 
     public TransactionMatrix(@NotNull ArrayList<Element> listElement)
     {
-        for (int i = 0; i < listElement.size(); i++)
+        OrderedTransaction orderedTransaction = new OrderedTransaction(listElement);
+    }
+
+    public ArrayList<Element> getTransactionNumber(int transactionNumber)
+    {
+        // TODO
+        return new ArrayList<Element>();
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private class OrderedTransaction
+    {
+        private ArrayList<Element> listOfElement;
+
+        private OrderedTransaction(@NotNull ArrayList<Element> listOfElement)
         {
-            int transactionNumber = listElement.get(i).getTransactionNumber();
-
-            if( matrix[i][0] == null )
+            try
             {
-
+                this.setListOfElement(listOfElement);
             }
+            catch (Exception e)
+            {
+                Utilities.printError(String.valueOf(e));
+            }
+        }
+
+        private void setListOfElement(@NotNull ArrayList<Element> listOfElement) throws Exception
+        {
+            int previousTransactionNumber = -1;
+
+            for(Element element : listOfElement)
+            {
+                //Given that we retrieve characters from the arguments and that we have a strict policy, there can't be any minus
+                if(previousTransactionNumber == -1)
+                    previousTransactionNumber = element.getTransactionNumber();
+                else if(element.getTransactionNumber() != previousTransactionNumber)
+                    throw new Exception(element + " doesn't belong to this ordered sequence");
+            }
+
+            this.listOfElement = listOfElement;
         }
     }
 }
